09d0e6701e6b78a50db489b02be0c35be9d19212
侑介 <spark9487111@gmail.com>
2025-03-19 00:16:57 +0800

    style: format

--- src/bcuzip/file_select.rs
+++ src/bcuzip/file_select.rs
@@ -3,7 +3,11 @@ use std::path::PathBuf;
 use crate::functions::file_selector::file_dialog;
 
 pub fn selectfile() -> Option<PathBuf> {
-    file_dialog(true, Some("BCUZIP files".to_string()), Some("bcuzip".to_string()))
+    file_dialog(
+        true,
+        Some("BCUZIP files".to_string()),
+        Some("bcuzip".to_string()),
+    )
 }
 
 pub fn selectfolder() -> Option<PathBuf> {
