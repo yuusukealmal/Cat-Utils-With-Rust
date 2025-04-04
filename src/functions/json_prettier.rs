69611298482383868d0acf8264c7efe5192fbcbf
yuusukealmal <spark9487111@gmail.com>
2025-04-04 22:47:22 +0800

    style: rename arg

--- src/functions/json_prettier.rs
+++ src/functions/json_prettier.rs
@@ -2,7 +2,7 @@ use serde::Serialize;
 use serde_json::{ser::PrettyFormatter, Serializer};
 
 pub fn indent_json(
-    data: &serde_json::Map<String, serde_json::Value>,
+    json: &serde_json::Map<String, serde_json::Value>,
 ) -> Result<String, Box<dyn std::error::Error>> {
     let formatter = PrettyFormatter::with_indent(b"    ");
 
@@ -10,7 +10,7 @@ pub fn indent_json(
 
     let mut ser = Serializer::with_formatter(&mut buf, formatter);
 
-    data.serialize(&mut ser)?;
+    json.serialize(&mut ser)?;
 
     Ok(String::from_utf8(buf)?)
 }
